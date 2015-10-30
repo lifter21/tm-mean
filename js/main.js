e1bb1cc028711e824da611dcad028341ed7037e5
Alexander Bondarenko <bondarenko.alexander.13@gmail.com>
2015-10-30 17:17:50 +0200

    complete comments crud

--- js/main.js
+++ js/main.js
@@ -1,46 +1,134 @@
 $('document').ready(function () {
-    var taskField = $('.task-field')
-    var taskTemplate = $('.task-template').text()
+    var tasksArray = []
+
+    var taskInputField = $('.task-field')
+    var taskTemplateText = $('.task-template').text()
+    var taskInViewIndex = 0
+    var commentTextField = $('.comment-text-field')
+    var commentUserField = $('.comment-user-field')
+    var commentTemplateText = $('.comment-template').text()
 
     $('.task-form').submit(function (event) {
         event.preventDefault()
-
-        var taskSection = $(taskTemplate)
-        var taskFieldVal = taskField.val()
-
-        if (taskFieldVal !== '') {
-            $('.task-list').append(taskSection)
-            taskSection.find('.task-list-text').text(taskFieldVal)
-            taskField.val('')
-
-            taskSection.find('.task-remove').click(function () {
-                taskSection.remove()
-            })
-
-            taskSection.find('.task-edit').click(function () {
-                var taskText = taskSection.find('.task-list-text').text()
-                taskSection.find('.task-list-text, .task-remove, .task-edit').hide()
-                taskSection.find('.task-edit-field, .task-cancel, .task-save').show()
-                taskSection.find('.task-edit-field').val(taskText)
-            })
-
-            taskSection.find('.task-save').click(function () {
-                var taskText = taskSection.find('.task-edit-field').val()
-                if (taskText !== '') {
-                    taskSection.find('.task-list-text, .task-remove, .task-edit').show()
-                    taskSection.find('.task-edit-field, .task-cancel, .task-save').hide()
-                    taskSection.find('.task-list-text').text(taskText)
-                }
-
-            })
-
-            taskSection.find('.task-cancel').click(function () {
-                taskSection.find('.task-edit, .task-remove, .task-list-text').show()
-                taskSection.find('.task-save, .task-cancel, .task-edit-field').hide()
-            })
-
+        var taskValue = taskInputField.val()
+        if(taskValue !== ''){
+            tasksArray.push({text: taskValue, comments: []})
+            taskInputField.val('')
+            displayTasks()
         }
-
-
     })
+
+    function addTask(task) {
+        var taskIndex = tasksArray.indexOf(task)
+        var $task = $(taskTemplateText)
+        $('.task-list').append($task)
+        $task.find('.task-list-text').text((taskIndex + 1) + '. ' + task.text)
+        $task.find('.task-remove').click(function () {
+            tasksArray.splice(taskIndex, 1)
+            displayTasks()
+        })
+
+        $task.find('.task-edit').click(function () {
+            $task.find('.task-list-text, .task-comments-button, .task-remove, .task-edit').hide()
+            $task.find('.task-edit-field, .task-save, .task-cancel').show()
+            $task.find('.task-edit-field').val(task.text)
+        })
+
+        $task.find('.task-cancel').click(function () {
+            $task.find('.task-list-text, .task-comments-button, .task-remove, .task-edit').show()
+            $task.find('.task-edit-field, .task-save, .task-cancel').hide()
+            $task.find('.task-edit-field').val('')
+        })
+
+        $task.find('.task-save').click(function () {
+            //var newValue = $task.find('.task-edit-field').val()
+            tasksArray[taskIndex].text = $task.find('.task-edit-field').val()
+            $task.find('.task-list-text, .task-comments-button, .task-remove, .task-edit').show()
+            $task.find('.task-edit-field, .task-save, .task-cancel').hide()
+            displayTasks()
+        })
+
+        $task.find('.task-comments').click(function () {
+            taskInViewIndex = taskIndex
+            $('.comments-section').show()
+            $('.tasks-section').hide()
+            displayComments(task)
+
+        })
+    }
+
+    $('.comment-form').submit(function (event) {
+        event.preventDefault()
+        var commentTextValue = commentTextField.val()
+        var commentUserValue = commentUserField.val()
+        if(commentTextValue !== '' && commentUserValue !== ''){
+            tasksArray[taskInViewIndex].comments.push({text: commentTextValue, user: commentUserValue})
+            $(commentUserField).val('')
+            $(commentTextField).val('')
+            displayComments(tasksArray[taskInViewIndex])
+        }
+    })
+
+    $('.back-to-task-list').click(function () {
+        $('.comments-section').hide()
+        $('.tasks-section').show()
+        commentTextField.val('')
+        commentUserField.val('')
+        taskInputField.val('')
+    })
+
+    function displayTasks() {
+        $('.task-list').empty()
+        $.each(tasksArray, function (index, task) {
+            addTask(task);
+        })
+    }
+
+    function addComment(task, comment) {
+        var taskIndex = tasksArray.indexOf(task)
+        var commentIndex = task.comments.indexOf(comment)
+        var $comment = $(commentTemplateText)
+        $('.comments-list').append($comment)
+        $comment.find('.task-comment-text').text((taskIndex+1) + '.' + (commentIndex+1) + '. ' + comment.text)
+        $comment.find('.task-comment-user-text').text('Created by:' + comment.user)
+
+        $comment.find('.task-comment-remove').click(function () {
+            tasksArray[taskIndex].comments.splice(commentIndex, 1)
+            displayComments(task)
+        })
+
+        $comment.find('.task-comment-edit').click(function () {
+            $comment.find('.task-comment-text, .task-comment-remove, .task-comment-edit').hide()
+            $comment.find('.task-comment-edit-field, .task-comment-save, .task-comment-cancel').show()
+            $comment.find('.task-comment-edit-field').val(comment.text)
+        })
+
+        $comment.find('.task-comment-cancel').click(function () {
+            $comment.find('.task-comment-text, .task-comments-button, .task-comment-remove, .task-comment-edit').show()
+            $comment.find('.task-comment-edit-field, .task-comment-save, .task-comment-cancel').hide()
+            $comment.find('.task-comment-edit-field').val('')
+        })
+
+        $comment.find('.task-comment-save').click(function () {
+            tasksArray[taskIndex].comments[commentIndex].text = $comment.find('.task-comment-edit-field').val()
+            $comment.find('.task-comment-text, .task-comment-remove, .task-comment-edit').show()
+            $comment.find('.task-comment-edit-field, .task-comment-save, .task-comment-cancel').hide()
+            displayComments(task)
+
+        })
+
+    }
+
+    function displayComments(task) {
+        var commentsTaskIndex = tasksArray.indexOf(task)
+        $('.comments-list').empty()
+        console.log(commentsTaskIndex);
+        console.log(tasksArray);
+        console.log(task);
+        $.each(tasksArray[commentsTaskIndex].comments, function (index, comment) {
+            addComment(task, comment);
+        })
+    }
+
 })
+
